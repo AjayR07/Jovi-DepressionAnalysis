e324b69bedf13af91fe893171471b52388f7f52c
Abinash S <s.abinash@outlook.com>
2021-02-01 22:42:55 +0530

    Node App Initialization

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require('express')
+
+let app = express();
+
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+app.listen(3000, function () {
+    console.log("Running Node on port " + 3000);
+});
+
+
