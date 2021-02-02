5e5cea5ed6249e9c6e2868f4e5a35968e83209e5
Abinash S <s.abinash@outlook.com>
2021-02-02 14:37:57 +0530

    Rest API complete sample done

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,61 @@
 const express=require('express')
 const router=express.Router()
+const jovi=require('../models/jovi.js')
+const getUser=require('../middleware/getUser')
 
 // Getting all
-router.get('/',(req,res)=>{
-    res.send('Getting all')
-})
-// Getting One
-router.get('/:id',(req,res)=>{
+router.get('/',async (req,res)=>{
+    try{
+        const allUsers=await jovi.find()
+        res.json(allUsers)
+    }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
 
 })
+// Getting One
+router.get('/:id',getUser,(req,res)=>{
+    res.json(res.user)
+})
 // Creating one
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
+    const user=new jovi({
+        name: req.body.name,
+        age: req.body.age
+    })
+    try{
+        const newUser=await user.save()
+        res.status(201).json(newUser)
+    }
+    catch(err){
+        res.status(400).json({message:err.message})
+    }
 
 })
 // Updating One
-router.patch('/:id',(req,res)=>{
-
+router.patch('/:id',getUser,async (req,res)=>{
+    if(req.body.name!=null){
+        res.user.name=req.body.name
+    }
+    if(req.body.age!=null){
+        res.user.age=req.body.age
+    }
+    try{
+        const updatedUser=await res.user.save()
+        res.json(updatedUser)
+    }catch (e) {
+        res.status(400).json({message:e.message})
+    }
 })
 // Deleting one
-router.delete('/:id',(req,res)=>{
-
+router.delete('/:id',getUser,async (req,res)=>{
+    try{
+        await res.user.remove()
+        res.status(204).json({message:'User Deleted Successfully'})
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
 })
 
 
